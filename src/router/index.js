0cb4852be884c456281bdd35affaa7b7ec4a5fd5
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-08-06 14:18:20 +0200

    header done

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ListView from '../views/ListView.vue'
+import FilterView from '@/views/FilterView.vue'
+import AddSpeciesView from '@/views/AddSpeciesView.vue'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/h',
+      path: '/',
       name: 'home',
       component: HomeView
     },
-  /*   {
-      path: '/about',
-      name: 'about',
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
+    },
+    {
+      path: '/list',
+      name: 'list',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Dashboard.vue')
-    } */
+      component: ListView
+    },
+    {
+      path: '/filter',
+      name: 'filter',
+      component: FilterView
+    },
+    {
+      path: '/addspecies',
+      name: 'addspecies',
+      component: AddSpeciesView
+    },
   ]
 })
 
---
+++ src/views/AddSpeciesView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+import HeaderOtherPages from '@/components/HeaderOtherPages.vue';
+
+</script>
+
+<template>
+  <main>
+    <HeaderOtherPages />
+  </main>
+ 
+</template>
