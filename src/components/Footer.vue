a1390843e44228eb0b73646542408b4e4081ac3e
piperiver97 <feliperiveros_123@gmail.com>
2024-08-09 12:49:35 +0200

    responsive finished

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,30 @@
 <template>
+  <div class="page-container">
+    <div class="content">
+      <!-- Tu contenido aquí -->
+    </div>
+  </div>
   <footer class="footer mt-auto py-3 border-top">
     <div class="container d-flex flex-wrap justify-content-between align-items-center">
-      <div class="d-flex align-items-center mb-3 mb-md-0">
-        <a href="/" class="me-2 text-muted text-decoration-none lh-1">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
           <svg class="bi" width="30" height="24"></svg>
         </a>
-        <span class="text-muted" id="zoocoders">© Zoocoders</span>
+        <span class="mb-3 mb-md-0 text-muted" id="zoocoders">© Zoocoders</span>
       </div>
 
-      <ul class="nav list-unstyled mb-0 social-icons d-flex">
-        <li class="ms-3">
+      <ul class="nav col-md-4 list-unstyled mb-0 social-icons" style="display: flex;">
+        <li class="mb-2">
           <a class="text-muted" href="#" aria-label="Instagram">
             <img src="/src/components/icons/Instagram.svg" alt="Instagram" width="24" height="24">
           </a>
         </li>
-        <li class="ms-3">
+        <li class="mb-2">
           <a class="text-muted" href="#" aria-label="Facebook">
             <img src="/src/components/icons/Facebook.svg" alt="Facebook" width="24" height="24">
           </a>
         </li>
-        <li class="ms-3">
+        <li class="mb-2">
           <a class="text-muted" href="#" aria-label="Twitter">
             <img src="/src/components/icons/Twitter.svg" alt="Twitter" width="24" height="24">
           </a>
@@ -28,47 +33,40 @@
     </div>
   </footer>
 </template>
+
 <style scoped>
-  html, body {
-    height: 100%;
-    margin: 0;
-  }
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
 
-  #app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
+.page-container {
+  display: flex;
+  flex-direction: column;
+  
+}
 
-  main {
-    flex-grow: 1;
-  }
+.content {
+  flex: 1;
+}
 
-  .footer {
-    background-color: #D9D9D9;
-    color: black !important;
-    width: 100%;
-    margin-top: auto;
-  }
+.footer {
+  background-color: #D9D9D9;
+  color: black !important;
+  width: 100%;
+  margin-top: auto;
+}
 
-  #zoocoders {
-    font-size: 30px;
-  }
+#zoocoders {
+  font-size: 30px;
+}
 
-  .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-  }
-
-  @media (min-width: 576px) {
-    .container {
-      flex-direction: row;
-      justify-content: space-between;
-      text-align: left;
-    }
-  }
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
 .text-muted {
   color: black !important;
@@ -82,32 +80,30 @@
   fill: currentColor;
 }
 
-  .nav {
-    padding: 0;
+.nav {
+  padding: 0;
+}
+
+.social-icons li {
+  display: flex;
+  justify-items: baseline;
+}
+
+.social-icons li img {
+  display: block;
+}
+
+@media (max-width: 767.98px) {
+  .footer {
+    padding: 1rem;
+  }
+
+  #zoocoders {
+    font-size: 20px;
   }
 
   .social-icons {
-    display: flex;
     justify-content: center;
-    padding-top: 1rem;
   }
-
-  @media (min-width: 576px) {
-    .social-icons {
-      justify-content: flex-end;
-      padding-top: 0;
-    }
-  }
-
-  .social-icons li {
-    display: flex;
-    justify-content: center;
-    margin-left: 15px;
-  }
-
-  .social-icons li img {
-    display: block; 
-  }
+}
 </style>
-
-
